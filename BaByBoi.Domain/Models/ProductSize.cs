231a12ccc977affe048fb234036ca00ecd712d1f
SE173453-PhuocTan <phuoctan0206@gmail.com>
2024-07-15 23:28:41 +0700

    fix: update and delete product in admin role

--- BaByBoi.Domain/Models/ProductSize.cs
+++ BaByBoi.Domain/Models/ProductSize.cs
@@ -11,7 +11,7 @@ namespace BaByBoi.Domain.Models
         }
 
         public double? Price { get; set; }
-        public int SizeId { get; set; }
+        public int? SizeId { get; set; }
         public int ProductId { get; set; }
         public int? Quantity { get; set; }
 
