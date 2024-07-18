eb3dead2df3ff640bfa7b1b3a84bccc6692fc150
SE173453-PhuocTan <phuoctan0206@gmail.com>
2024-07-18 21:12:31 +0700

    feat: deploy project

--- BaByBoi_Project/Program.cs
+++ BaByBoi_Project/Program.cs
@@ -81,12 +81,9 @@ builder.Services.AddAuthentication(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-}
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseSession();
